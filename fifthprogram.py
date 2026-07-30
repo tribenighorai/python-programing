5d7a3a2acb8df8d3689cfa4699909f5a4f10c803
tribenighorai <tapasghori9@gmail.com>
2026-07-30 22:42:00 +0530

    Create fifthprogram.py

---
+++ fifthprogram.py
@@ -0,0 +1,4 @@
+#wap to input 2 int numb., a and b, print true if a is greater than  or equal to b. if not print False.
+a=int(input("enter the first num.:"))
+b=int(input("enter the second num.:"))
+print(a>=b) 
