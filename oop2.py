8818df0a7ed84fde585b861549ca480eb0ee9054
tribenighorai <tapasghori9@gmail.com>
2026-07-30 22:44:59 +0530

    Create oop2.py

---
+++ oop2.py
@@ -0,0 +1,19 @@
+class Student:
+    
+     #constructor define
+     
+     #defalt constructor
+     #def __init__ (self):
+     #pass
+
+
+     #paramitarize constructor
+    def __init__ (self,fullname,mark):
+        self.name=fullname
+        self.mark=mark
+        print("adding new student id database..")
+        
+s1=Student("Tribeni",67)
+print(s1.name,s1.mark)
+s2=Student("piu",89)
+print(s2.name,s2.mark)
