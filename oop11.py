d813e686aa8380c1aa30a9acbc2fb234e024c731
Tribeni Ghorai <tapasghori9@gmail.com>
2026-08-03 22:36:34 +0530

    Create oop11.py

---
+++ oop11.py
@@ -0,0 +1,9 @@
+class Account:
+    def __init__ (self,acc_no,acc_pass):
+        self.acc_no=acc_no
+        self.__acc_pass=acc_pass
+    def reset_pass(self):
+        print(self.__acc_pass)
+acc1=Account("12334","eehdjj")
+print(acc1.acc_no)
+print(acc1.reset_pass())
