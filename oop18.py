79d46a19c4c761fa600066c94b41e534b7049bc8
Tribeni Ghorai <tapasghori9@gmail.com>
2026-08-08 11:46:32 +0530

    Create oop18_proparty.py

---
+++ oop18.py
@@ -0,0 +1,18 @@
+class Student:
+
+    def __init__(self,phy,chem,math):
+        self.phy=phy
+        self.chem=chem
+        self.math=math
+    @property
+    def parcentage(self):
+        self.par=(self.phy+self.chem+self.math)/3
+        print(self.par)  
+
+   
+S1=Student(98,99,89)
+print(S1.parcentage)
+
+#printing mistek actual physics mark is 77
+S1.phy=77
+print(S1.parcentage)
